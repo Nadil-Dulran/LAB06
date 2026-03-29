02c76f9558244ec2e41349350316d15abb27987c
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-30 00:38:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LAB06
+Android applications by building examples that demonstrate Services, Broadcast Receivers, and Content Providers.
---
+++ app/src/main/java/com/example/lab06/MyStartedService.kt
@@ -0,0 +1,38 @@
+package com.example.lab06
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.widget.Toast
+import kotlinx.coroutines.*
+
+class MyStartedService : Service() {
+
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show()
+
+        scope.launch {
+            while (isActive) {
+                delay(5000L)
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(applicationContext, "Service Running", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        scope.cancel()
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
+    }
+}
